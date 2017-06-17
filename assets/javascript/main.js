0d170213cdb7fd06554e3a11f31d4afb15063d20
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-06-16 21:42:29 -0500

    added page, button styles and font; added logic to auto load gifs for new button and to add new button with enter key

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,27 @@
+//Define object to hold GIFtastic properties and methods
+
 var giftastic = {
-	limit : 12,
-	searchfor : ['star wars', 'puppies', 'kittens'],
-	rating : 'pg13',
-	result: null,
+	limit : 12, //no of GIFs to request
+	//holds the search terms for initial button display
+	searchfor : ['falling', 'yoda', 'flying dog', 'office', 'face palm','parkour', 'donald duck', 'penguin'],
+	rating : 'pg13', //holds rating under which to request GIFs
+	result: null, //to hold the API result
+
+	/*function to add the user entered button and 
+	to display buttons and result GIFS for currently added button*/ 
+	addButton: function()
+	{
+		var add=$('#userip').val();
+		if(add!==''){
+			this.searchfor.push(add);
+			this.displayButtons();
+			this.getGifs(add);
+		}
+		$('#userip').val('');
+	},
+
+	/*function to call the API to get the results
+	 when a button is clicked*/
 	getGifs: function(dispFor)
 	{
 		var searchapi="http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+dispFor+"&limit="+this.limit+"&rating="+this.rating;
@@ -14,12 +33,15 @@ var giftastic = {
 				this.displayGifs();
 			}.bind(this));
 	},
+
+	//function to display GIFs after API response
 	displayGifs: function(){
 			$('#gifimg').empty();
 			for(i=0; i<this.limit; i++){
 				var imdiv=$('<div>');
-				var rtng=$('<h4>');
+				var rtng=$('<p>');
 				var im=$('<img>');
+				rtng.addClass('cs-rtng');
 				im.addClass('js-img cs-img');
 				im.attr({
 					'state': 'still',
@@ -34,13 +56,15 @@ var giftastic = {
 				$('#gifimg').append(imdiv);
 			}
 	},
+
+	//function to re-render the buttons after a new button is added
 	displayButtons: function()
 	{
 		$('#gifbuttons').empty();
 
 		for(i=0; i<this.searchfor.length; i++){
 			var b = $('<button>');
-			b.addClass('js-gifbtn cs-gifbtn');
+			b.addClass('js-gifbtn cs-gifbtn col-md-2 col-xs-6');
 			b.attr('btn-val', this.searchfor[i]);
 			b.text(this.searchfor[i]);
 			$('#gifbuttons').append(b);
@@ -51,22 +75,30 @@ var giftastic = {
 
 
 $(document).ready(function(){
+	//create new giftastic variable
 	var gen=giftastic;
 	gen.displayButtons();
 	
+	//jQuery to identify the button clicked and display the gifs
 	$('#gifbuttons').on('click', '.js-gifbtn', function(){
 		var dispFor=$(this).attr('btn-val');
 		gen.getGifs(dispFor);
 	});
 
+	//jQuery to identify when Add button is clicked
 	$('#add-btn').on('click', function(){
-		console.log('i am in add btn');
-		var add=$('#userip').val();
-		gen.searchfor.push(add);
-		gen.displayButtons();
+		gen.addButton();
 	});
 
-	$('#gifimg').on('click', '.js-img', function(){
+	//jQuery to identify if enter key is pressed instead of add button
+	$('#userip').on('keypress', function(e){
+		if (e.which===13){
+			gen.addButton();
+		}
+	});
+
+	//jQuery to toggle play_pause of GIFs on click
+	$('#gifimg').on('click', '.js-img', function(e){
 		var curImg;
 		if ($(this).attr('state')==='still'){
 			curImg=$(this).attr('animate_src');
@@ -78,7 +110,6 @@ $(document).ready(function(){
 			$(this).attr('src', curImg);
 			$(this).attr('state','still')
 		}
-		
 	});
 
 });
